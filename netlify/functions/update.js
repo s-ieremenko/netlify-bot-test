29f7286c87fc035dfc7496ad9e4349b755122a16
s-ieremenko <ieremenko.sveta@gmail.com>
2022-11-23 13:37:33 +0100

    added sendMessage.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,6 +1,7 @@
 const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
+    console.log(event.body)
     const { message } = JSON.parse(event.body);
     await sendMessage(message.chat.id, "I got your message!");
     return { statusCode: 200 };
