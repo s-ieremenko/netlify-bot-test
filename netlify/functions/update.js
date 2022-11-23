119af463d186b7ac59d115f3093d275395738eba
s-ieremenko <ieremenko.sveta@gmail.com>
2022-11-23 13:37:33 +0100

    added sendMessage.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,11 +1,8 @@
-const axios = require("axios").default;
-exports.handler = async (event) => {
-    console.log("Received an update from Telegram!", event.body);
-    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-        chat_id: JSON.parse(event.body).message.chat.id,
-        text: "I got your message!",
-    });
+const sendMessage = require("../../sendMessage");
 
-    return { statusCode: 200 }
+exports.handler = async (event) => {
+    const { message } = JSON.parse(event.body);
+    await sendMessage(message.chat.id, "I got your message!");
+    return { statusCode: 200 };
 };
 
